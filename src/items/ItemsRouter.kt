db2284b62cfeb1face8999fe90fa2840ed659d19
bvlion <iwai.satoshi@zaim.co.jp>
2020-09-29 09:14:16 +0900

    items の認証設定漏れ

--- src/items/ItemsRouter.kt
+++ src/items/ItemsRouter.kt
@@ -16,36 +16,37 @@ object ItemsRouter {
     val controller: ItemsController by inject()
 
     route("/items") {
-
-      get("/list") {
-        call.respond(transaction { controller.getItems(call.principal<Util.AuthUser>()!!.id) })
-      }
-
-      put("/parents") {
-        call.receive<Array<ItemsModel.PostParents>>().let {
-          call.respond(transaction {
-            controller.setParentIds(it, call.principal<Util.AuthUser>()!!.id)
-          })
+      authenticate {
+        get("/list") {
+          call.respond(transaction { controller.getItems(call.principal<Util.AuthUser>()!!.id) })
         }
-      }
 
-      post("/save") {
-        call.receive<Array<ItemsModel.PostItem>>().let {
-          call.respond(transaction {
-            controller.saveItems(
-              call.principal<Util.AuthUser>()!!.id,
-              it
-            )
-          })
-        }
-      }
-
-      delete("/delete") {
-        call.principal<Util.AuthUser>()!!.run {
-          call.receive<Array<ItemsModel.DeleteRequest>>().let {
+        put("/parents") {
+          call.receive<Array<ItemsModel.PostParents>>().let {
             call.respond(transaction {
-              controller.deleteItems(it)
+              controller.setParentIds(it, call.principal<Util.AuthUser>()!!.id)
             })
+          }
+        }
+
+        post("/save") {
+          call.receive<Array<ItemsModel.PostItem>>().let {
+            call.respond(transaction {
+              controller.saveItems(
+                call.principal<Util.AuthUser>()!!.id,
+                it
+              )
+            })
+          }
+        }
+
+        delete("/delete") {
+          call.principal<Util.AuthUser>()!!.run {
+            call.receive<Array<ItemsModel.DeleteRequest>>().let {
+              call.respond(transaction {
+                controller.deleteItems(it)
+              })
+            }
           }
         }
       }
