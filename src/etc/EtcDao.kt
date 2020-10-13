66735cd951477222a42a2f77bf423e2814ca098c
bvlion <iwai.satoshi@zaim.co.jp>
2020-10-13 21:59:26 +0900

    よくある質問の準備

--- src/etc/EtcDao.kt
+++ src/etc/EtcDao.kt
@@ -2,7 +2,7 @@ package net.ambitious.sharebookmarks.etc
 
 import org.jetbrains.exposed.dao.IntIdTable
 
-object EtcDao : IntIdTable("etc") {
+object EtcDao : IntIdTable("term") {
   val type = integer("type")
   val lang = text("lang")
   val message = text("message")
